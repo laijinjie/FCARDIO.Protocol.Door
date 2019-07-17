d4b5b9e8c80667eec71ead2ef4ea6b349c807ad7
徐铭康 <justicesai@163.com>
2019-07-17 14:09:17 +0800

    no commit message

--- FCARDIO.Protocol.Door/FC8800/Transaction/05TransactionDatabaseReadIndex/WriteTransactionDatabaseReadIndex.cs
+++ FCARDIO.Protocol.Door/FC8800/Transaction/05TransactionDatabaseReadIndex/WriteTransactionDatabaseReadIndex.cs
@@ -13,7 +13,7 @@ namespace FCARDIO.Protocol.Door.FC8800.Transaction.TransactionDatabaseReadIndex
     /// <summary>
     /// 更新记录指针
     /// </summary>
-    public class WriteTransactionDatabaseReadIndex : FC8800Command
+    public class WriteTransactionDatabaseReadIndex : FC8800Command_WriteParameter
     {
         /// <summary>
         /// 初始化命令结构
@@ -39,45 +39,9 @@ namespace FCARDIO.Protocol.Door.FC8800.Transaction.TransactionDatabaseReadIndex
         /// </summary>
         protected override void CreatePacket0()
         {
-            Packet(0x08, 0x03, 0x00, 0x06, getCmdData());
-        }
-
-        /// <summary>
-        /// 获取参数结构的字节编码
-        /// </summary>
-        /// <returns></returns>
-        private IByteBuffer getCmdData()
-        {
             WriteTransactionDatabaseReadIndex_Parameter model = _Parameter as WriteTransactionDatabaseReadIndex_Parameter;
-            var acl = _Connector.GetByteBufAllocator();
-            var buf = acl.Buffer(model.GetDataLen());
-            model.GetBytes(buf);
-            return buf;
+            Packet(0x08, 0x03, 0x00, 0x06, model.GetBytes(GetNewCmdDataBuf(model.GetDataLen())));
         }
 
-        /// <summary>
-        /// 处理返回值
-        /// </summary>
-        /// <param name="oPck"></param>
-        protected override void CommandNext1(OnlineAccessPacket oPck)
-        {
-            return;
-        }
-
-        /// <summary>
-        /// 命令重发时需要处理的函数
-        /// </summary>
-        protected override void CommandReSend()
-        {
-            return;
-        }
-
-        /// <summary>
-        /// 命令释放时需要处理的函数
-        /// </summary>
-        protected override void Release1()
-        {
-            return;
-        }
     }
 }
--- FCARDIO.Protocol.Door/FC8800/Transaction/06WriteTransactionDatabaseWriteIndex/WriteTransactionDatabaseWriteIndex.cs
+++ FCARDIO.Protocol.Door/FC8800/Transaction/06WriteTransactionDatabaseWriteIndex/WriteTransactionDatabaseWriteIndex.cs
@@ -15,7 +15,7 @@ namespace FCARDIO.Protocol.Door.FC8800.Transaction.WriteTransactionDatabaseWrite
     /// 修改指定记录数据库的写索引
     /// 记录尾地址
     /// </summary>
-    public class WriteTransactionDatabaseWriteIndex :FC8800Command
+    public class WriteTransactionDatabaseWriteIndex : FC8800Command_WriteParameter
     {
         /// <summary>
         /// 初始化命令结构
@@ -41,46 +41,9 @@ namespace FCARDIO.Protocol.Door.FC8800.Transaction.WriteTransactionDatabaseWrite
         /// </summary>
         protected override void CreatePacket0()
         {
-            Packet(0x08, 0x03, 0x01, 0x05, getCmdData());
-        }
-
-        /// <summary>
-        /// 获取参数结构的字节编码
-        /// </summary>
-        /// <returns></returns>
-        private IByteBuffer getCmdData()
-        {
             WriteTransactionDatabaseWriteIndex_Parameter model = _Parameter as WriteTransactionDatabaseWriteIndex_Parameter;
-            var acl = _Connector.GetByteBufAllocator();
-            var buf = acl.Buffer(model.GetDataLen());
-            model.GetBytes(buf);
-            return buf;
+            Packet(0x08, 0x03, 0x01, 0x05, model.GetBytes(GetNewCmdDataBuf(model.GetDataLen())));
         }
 
-        /// <summary>
-        /// 处理返回值
-        /// </summary>
-        /// <param name="oPck"></param>
-        protected override void CommandNext1(OnlineAccessPacket oPck)
-        {
-            return;
-        }
-
-        /// <summary>
-        /// 命令重发时需要处理的函数
-        /// </summary>
-        protected override void CommandReSend()
-        {
-            return;
-        }
-
-        /// <summary>
-        /// 命令释放时需要处理的函数
-        /// </summary>
-        protected override void Release1()
-        {
-            return;
-        }
-        
     }
 }
