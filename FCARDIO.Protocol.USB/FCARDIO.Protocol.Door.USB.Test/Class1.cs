ffb00dace8679f414c76a024ee960c3727ab8d07
徐铭康 <justicesai@163.com>
2019-07-22 09:45:40 +0800

    创建 巡更帮，读卡器 工程

---
+++ FCARDIO.Protocol.USB/FCARDIO.Protocol.Door.USB.Test/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCARDIO.Protocol.Door.USB.Test
+{
+    public class Class1
+    {
+    }
+}
