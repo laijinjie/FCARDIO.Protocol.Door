d91f230a622b7dfdd2737c483f7fc2db128a926f
赖金杰 <2880150916@qq.com>
2021-02-23 17:24:00 +0800

    增加巡更棒的net5版本编译

--- DoNetDrive.Protocol.USB.Test/OfflinePatrol/Program.cs
+++ DoNetDrive.Protocol.USB.Test/OfflinePatrol/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace DoNetDrive.Protocol.USB.OfflinePatrol.Test
@@ -11,9 +12,24 @@ namespace DoNetDrive.Protocol.USB.OfflinePatrol.Test
         [STAThread]
         static void Main()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMain());
+        }
+
+        private static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            string sPath = AppDomain.CurrentDomain.BaseDirectory;
+            string sDllName = args.Name.Split(',')[0];
+            sPath = System.IO.Path.Combine(sPath, "DLL", $"{sDllName}.dll");
+            if (System.IO.File.Exists(sPath))
+            {
+                return Assembly.LoadFrom(sPath);
+            }
+            else
+                return null;
+
         }
     }
 }
